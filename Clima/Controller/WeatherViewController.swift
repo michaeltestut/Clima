ca5c9ce4d7290114f18e69c59dfe200bb1368942
Michael Testut <michael.testut@yahoo.com>
2023-01-06 18:15:19 -0800

    code refactoring

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -5,9 +5,9 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
     
-
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -21,8 +21,11 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         weatherManager.delegate=self
         searchTextField.delegate=self
     }
+}
 
+//MARK: - UITextFieldDelegate
 
+extension WeatherViewController: UITextFieldDelegate{
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
     }
@@ -47,6 +50,11 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         
     }
     
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController:WeatherManagerDelegate{
     func didUpdateWeather(_ weatherManager:WeatherManager, weather: WeatherModel) {
         DispatchQueue.main.async {
             self.temperatureLabel.text=weather.temperatureString
@@ -59,4 +67,4 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         print(error)
     }
 }
-
+    
